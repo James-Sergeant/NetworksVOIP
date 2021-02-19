25cba4101165a30942f04b80487d9a29e2c7ad4d
James Sergeant <57397095+James-Sergeant@users.noreply.github.com>
2021-02-19 14:25:09 +0000

    Threads

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,12 +2,18 @@ package com;
 
 import com.google.common.hash.Hashing;
 
+import javax.sound.sampled.LineUnavailableException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-    public static void main(String[] args) {
-        String test = "Sodetsd17";
-        System.out.println(Hashing.md5().hashString(test, Charset.defaultCharset()));
+    public static void main(String[] args) throws LineUnavailableException, SocketException, UnknownHostException {
+        Thread thread = new Thread(new Receiver());
+        Thread thread1 = new Thread(new Sender());
+
+        thread.start();
+        thread1.start();
     }
 }
